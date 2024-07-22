d3bcaecc7347c50099f86ab8e8d6f38a39e43226
manu <manuel.casillas54@gmail.com>
2024-07-22 12:31:41 +0200

    WIP. Refactor login

--- app/src/main/java/com/formation/mvvm_compose/login/Login.kt
+++ app/src/main/java/com/formation/mvvm_compose/login/Login.kt
@@ -12,12 +12,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.BasicText
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -38,19 +37,16 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.text.MultiParagraph
 import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextLayoutInput
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -59,7 +55,7 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withAnnotation
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
@@ -69,18 +65,20 @@ import com.formation.mvvm_compose.commons.BasicScreen
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun LoginRoot(vm: LoginViewModel = koinViewModel(), onLogin:() -> Unit) {
+fun LoginRoot(vm: LoginViewModel = koinViewModel(), onLogin: () -> Unit) {
     BasicScreen(content = {
         Login(vm.state, onLogin, vm::onLoginClick, vm::userValueChanged, vm::passValueChanged)
     })
 }
 
+@OptIn(ExperimentalTextApi::class)
 @Composable
-fun Login(state: LoginViewModel.UiState,
-          onLogin:()-> Unit,
-          onLoginClick: (user: String, pass: String) -> Unit,
-          userValueChanged: () -> Unit,
-          passValueChanged: () -> Unit
+fun Login(
+    state: LoginViewModel.UiState,
+    onLogin: () -> Unit,
+    onLoginClick: (user: String, pass: String) -> Unit,
+    userValueChanged: () -> Unit,
+    passValueChanged: () -> Unit
 ) {
 
     LaunchedEffect(state.loggedIn) {
@@ -88,81 +86,163 @@ fun Login(state: LoginViewModel.UiState,
     }
 
 
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
 
+        val context = LocalContext.current
 
-    Box(contentAlignment = Alignment.Center, modifier = Modifier.imePadding()) {
-
-
-        Column(
-            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.width(IntrinsicSize.Min)
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .padding(top = 16.dp)
         ) {
-            val focusManager = LocalFocusManager.current
+            Text(text = "Elige el idioma")
+        }
 
-            val context = LocalContext.current
+        Image(
+            painter = painterResource(id = R.drawable.ic_marvel),
+            contentDescription = null,
+            modifier = Modifier
+                .size(140.dp)
+                .weight(1f),
+        )
 
-            var user by rememberSaveable { mutableStateOf(value = "") }
-            var pass by rememberSaveable { mutableStateOf(value = "") }
-            val buttonEnabled = pass.isNotEmpty() && user.isNotEmpty()
+        Box(
+            contentAlignment = Alignment.Center, modifier = Modifier
+                .imePadding()
+                .weight(3.5f)
+        ) {
+
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.width(IntrinsicSize.Min)
+            ) {
+                val focusManager = LocalFocusManager.current
 
 
-            Image(
-                painter = painterResource(id = R.drawable.ic_marvel),
-                contentDescription = null,
-                modifier = Modifier.size(140.dp),
-            )
+                var user by rememberSaveable { mutableStateOf(value = "") }
+                var pass by rememberSaveable { mutableStateOf(value = "") }
+                val buttonEnabled = pass.isNotEmpty() && user.isNotEmpty()
 
+                StateUserTextField(
+                    value = user,
+                    onValueChange = {
+                        user = it
+                        userValueChanged()
+                    },
+                    isError = state.isUserError
+                )
 
-            StateUserTextField(
-                value = user,
-                onValueChange = {
-                    user = it
-                    userValueChanged()
-                },
-                isError = state.isUserError
-            )
-
-            Crossfade(targetState = state.isUserError) { isUserError ->
-                if (isUserError){
-                    Text(text = "User must contain @")
+                Crossfade(targetState = state.isUserError) { isUserError ->
+                    if (isUserError) {
+                        Text(text = "User must contain @")
+                    }
                 }
-            }
 
-            StatePasswordTextField(
-                value = pass,
-                onValueChange = {
-                    pass = it
-                    passValueChanged()
-                },
-                isError = state.isPassError
-            )
+                StatePasswordTextField(
+                    value = pass,
+                    onValueChange = {
+                        pass = it
+                        passValueChanged()
+                    },
+                    isError = state.isPassError
+                )
 
-            Crossfade(targetState = state.isPassError) { isUserError ->
-                if (isUserError){
-                    Text(text = "Password must contains 5 letters")
+                Crossfade(targetState = state.isPassError) { isUserError ->
+                    if (isUserError) {
+                        Text(text = "Password must contains 5 letters")
+                    }
                 }
-            }
 
-            Button(
-                enabled = buttonEnabled,
-                onClick = {
-                    focusManager.clearFocus()
-                    onLoginClick(user, pass)
-                },
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .fillMaxWidth(),
+                Button(
+                    enabled = buttonEnabled,
+                    onClick = {
+                        focusManager.clearFocus()
+                        onLoginClick(user, pass)
+                    },
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .fillMaxWidth(),
 
-                ) {
-                Text(text = "Login")
-            }
+                    ) {
+                    Text(text = "Login")
+                }
 
-            ClickablePartOfText {
-                Toast.makeText(context, "Estamos trabajando en ello", Toast.LENGTH_SHORT).show()
+                ClickablePartOfText(
+                    buildAnnotatedString {
+                        withStyle(style = SpanStyle(fontSize = 12.sp, color = Color.Gray)) {
+                            append("Has olvidado tus datos de inicio de sesión?")
+                        }
+
+                        withAnnotation("tag", "annotation") {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Gray,
+                                    fontSize = 12.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            ) {
+                                append("Obtén ayuda")
+                            }
+                        }
+                    }) {
+                    Toast.makeText(context, "Estamos trabajando en ello", Toast.LENGTH_SHORT).show()
+                }
             }
         }
+
+        Text("Login Social", modifier = Modifier.weight(1f))
+
+
+        Box(
+            modifier = Modifier.weight(1f)
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp) // Specify the height of the thin line
+                    .background(Color.LightGray)// Set the color of the line
+            )
+
+            Text(
+                text = " ",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+            )
+
+            ClickablePartOfText(
+                buildAnnotatedString {
+                    withStyle(style = SpanStyle(fontSize = 16.sp, color = Color.Gray)) {
+                        append("¿No tienes cuenta? ")
+                    }
+
+                    withAnnotation("tag", "annotation") {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Gray,
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        ) {
+                            append("Regístrate")
+                        }
+                    }
+                }, center = true) {
+                Toast.makeText(context, "Estamos trabajando en ello", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
     }
+
 }
 
 @Composable
@@ -192,7 +272,10 @@ private fun StateUserTextField(
         textStyle = TextStyle(
             color = if (isError) Color.Red else Color.Black
         ),
-        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next, keyboardType = KeyboardType.Email)
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Next,
+            keyboardType = KeyboardType.Email
+        )
     )
 }
 
@@ -254,47 +337,55 @@ private fun StatePasswordTextField(
     )
 }
 
-@OptIn(ExperimentalTextApi::class)
 @Composable
-fun ClickablePartOfText(onTextClick: () -> Unit) {
+fun ClickablePartOfText(annotatedString: AnnotatedString,center: Boolean = false,onTextClick: () -> Unit, ) {
 
     var textLayoutResult by remember { mutableStateOf<TextLayoutResult?>(null) }
 
-    val annotatedString = buildAnnotatedString {
-            withStyle(style = SpanStyle(fontSize = 12.sp, color = Color.Gray)) {
-                append("Has olvidado tus datos de inicio de sesión?")
-            }
+    var textAlign = TextAlign.Start
+    if (center) {
+        textAlign = TextAlign.Center
+    }
 
-            withAnnotation("tag", "annotation") {
-                withStyle(style = SpanStyle(color = Color.Gray, fontSize = 12.sp, fontWeight = FontWeight.Bold)) {
-                    append("Obtén ayuda")
-                }
-            }
-        }
-
-    BasicText(
+    Text(
         text = annotatedString,
-        onTextLayout = { it ->
+        lineHeight = 18.sp,
+        onTextLayout = {
             textLayoutResult = it
         },
+        textAlign = textAlign,
         modifier = Modifier
-            .padding(16.dp)
+            .conditional(center){
+                Modifier.fillMaxWidth().padding(16.dp)
+
+            }
             .pointerInput(Unit) {
 
                 detectTapGestures { offset ->
                     textLayoutResult?.let { layoutResult ->
                         val position = layoutResult.getOffsetForPosition(offset)
-                        annotatedString.getStringAnnotations(
-                            start = position,
-                            end = position,
-                        ).firstOrNull { annotation ->
-                            annotation.tag.startsWith("tag")
-                        }?.let { annotation ->
-                            val userId = annotation.item
-                            onTextClick()
-                        }
+                        annotatedString
+                            .getStringAnnotations(
+                                start = position,
+                                end = position,
+                            )
+                            .firstOrNull { annotation ->
+                                annotation.tag.startsWith("tag")
+                            }
+                            ?.let { annotation ->
+                                val userId = annotation.item  // could be use when a annotated string have more than one tag. Could it be differentiate with a when
+                                onTextClick()
+                            }
                     }
                 }
             }
     )
+}
+
+fun Modifier.conditional(condition : Boolean, modifier : Modifier.() -> Modifier) : Modifier {
+    return if (condition) {
+        then(modifier(Modifier))
+    } else {
+        this
+    }
 }
