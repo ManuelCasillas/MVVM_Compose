2462b0f66b6aa12cda3fa99b37f33d5f5dc68023
manu <manuel.casillas54@gmail.com>
2024-07-24 13:15:09 +0200

    Splash view added.

--- app/src/main/java/com/formation/mvvm_compose/di/module.kt
+++ app/src/main/java/com/formation/mvvm_compose/di/module.kt
@@ -1,12 +1,14 @@
 package com.formation.mvvm_compose.di
 
 import com.formation.mvvm_compose.login.LoginViewModel
+import com.formation.mvvm_compose.main.MainViewModel
 import org.koin.dsl.module
 import org.koin.androidx.viewmodel.dsl.viewModel
 
 
 val viewModelModules = module {
     viewModel { LoginViewModel() }
+    viewModel { MainViewModel() }
 }
 
 val useCaseModules = module { }
