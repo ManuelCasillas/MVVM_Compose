aebc6688973196277f9c95465235ff21d4fa6230
manu <manuel.casillas54@gmail.com>
2024-10-06 13:56:21 +0200

    Added basic detail to CharacterDetails

--- app/src/main/java/com/formation/mvvm_compose/screens/home/characters/characterDetails/CharactersDetails.kt
+++ app/src/main/java/com/formation/mvvm_compose/screens/home/characters/characterDetails/CharactersDetails.kt
@@ -1,15 +1,31 @@
 package com.formation.mvvm_compose.screens.home.characters.characterDetails
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.formation.domain.model.Character
 import com.formation.mvvm_compose.navigation.mainNavigation.MainRoutes
 import com.formation.mvvm_compose.utils.ShowLoadingView
@@ -42,27 +58,70 @@ fun CharacterDetails(
 
 
 @Composable
-fun ShowCharacterInfo(characterInfo: Character){
+fun ShowCharacterInfo(characterInfo: Character) {
+    val scrollState = rememberScrollState()
 
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text("¿Este character es favorito? ->  ${characterInfo.favorite}", textAlign = TextAlign.Center)
-    }
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState), // Apply vertical scroll
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
 
+    ) {
+
+        AsyncImage(
+            model = characterInfo.thumbnail,
+            contentDescription = characterInfo.title,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.aspectRatio(16 / 9f)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+
+            Text(
+                characterInfo.title,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.align(Alignment.Center)
+            )
+
+
+            val favoriteImage = if (characterInfo.favorite) {
+                Icons.Default.Favorite
+            } else {
+                Icons.Default.FavoriteBorder
+            }
+            Icon(
+                imageVector = favoriteImage,
+                contentDescription = null,
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+            )
+        }
+
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(characterInfo.description, Modifier.padding(16.dp))
+
+    }
 }
 
-@Composable
-fun ShowErrorMessage(msg: String) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(msg, textAlign = TextAlign.Center)
+    @Composable
+    fun ShowErrorMessage(msg: String) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(msg, textAlign = TextAlign.Center)
+        }
     }
-}
-
 
